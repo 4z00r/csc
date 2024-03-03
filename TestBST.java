5cc5bd899f46e931c3359bd1d3498c0eb55677a0
4z00r <161839275+4z00r@users.noreply.github.com>
2024-03-03 12:31:44 +0200

    Add files via upload

--- TestBST.java
+++ TestBST.java
@@ -6,7 +6,11 @@ public class TestBST {
          tree.findConfidence(tree, "insurance broker", "An insurance broker is a broker");
          //Node node = tree.find("insurance broker");
          //System .out.println(node.line.getSentence()+ "\t" + node.line.getConfidencelvl());
-
-
+        //  tree.delete("insurance broker");
+        //  kb ib = new kb("insurance broker", "an insurance broker doesnt break things","0.9" );
+        //  tree.insert(ib);
+        //  tree.find(tree,"insurance broker");
+        tree.updateTerm("insurance broker", "i dont know what they do", "1",tree);
+        tree.find(tree,"insurance broker");
     }
 }
